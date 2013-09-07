866def12f60e786f7052bc7d98edcac2daf34126
Isabel <ibarrera@renttherunway.com>
2013-09-07 17:29:31 -0400

    more styles

--- app/assets/javascripts/fashion.js
+++ app/assets/javascripts/fashion.js
@@ -12,12 +12,26 @@ $(document).ready(function(){
 
 	$(document).delegate('#search', 'click', function(){
 
-		$('.search-box').hide();
-	})
+		$('.search-wrapper').hide();
+		$('.search-info').show();
+	});
+
+	setInterval(function(){
+		textTimer();
+	}, 3000);
 
 });
 
-function textTimer(){
+var textArray = ["See how things look on people like you.", "Shop items people have tried and loved.","Find your FashionMate and start shopping!"];
+var counter = 0;
 
-	$('.')
+function textTimer(){
+	console.log("Text timer called")
+	$('#search-text').html(textArray[counter]);
+
+	if(counter < textArray.length){
+		counter++
+	}else{
+		counter = 0;
+	}
 }
