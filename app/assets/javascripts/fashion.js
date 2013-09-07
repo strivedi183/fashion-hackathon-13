5fea7194718cb980f2b7fa0915eac5f4c2842154
Isabel <ibarrera@renttherunway.com>
2013-09-07 16:30:29 -0400

    sexy styles

--- app/assets/javascripts/fashion.js
+++ app/assets/javascripts/fashion.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 
+	$(document).delegate('#login', 'click', function(){
+		$('.opacity').show();
+		$('.login-view').show();
+	})
 
-
+	$(document).delegate('.opacity', 'click', function(){
+		$(this).hide();
+		$('.login-view').hide();
+	});
 
 });
+
+function textTimer(){
+
+	$('.')
+}
