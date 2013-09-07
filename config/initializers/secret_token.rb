f8299eaaf1a3862d7b6ac32970c8105379287352
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-09-07 10:51:01 -0400

    Initial skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FashionHackathon13::Application.config.secret_token = '859fa86cda9d224dd1b34c4f24a5603610a108a4a626d1a593a8810d9c3a02d46962deac044fede067bbd0d10b85883f73a12c306f2c31e8e3b815bda6ecc95c'
